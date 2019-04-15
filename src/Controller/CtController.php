386f0cb2fc1b889584c130335ddd3f7c83478f73
Mozart <husaynmozart@gmail.com>
2019-04-15 16:06:30 +0300

    profil actuel

--- src/Controller/CtController.php
+++ src/Controller/CtController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class CtController extends AbstractController
 {
@@ -24,13 +25,27 @@ class CtController extends AbstractController
     }
 
     /**
+     * @Route("/profil", name="ct_profil")
+     */
+    public function profil(){
+        //gestion de la vision de la profil utilisateur
+        $user = new User();
+        //$username = $this->container->get('security.context')->getToken()->getUser();
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
+        //$user = $repository->findOneBy(['username' => $username]);
+        return $this->render('ct/profil.html.twig', [
+            'users' => $user,
+        ]);
+    }
+
+    /**
      * @Route("/", name="ct_base")
      */
     public function rediriger()
     {
         if(!empty($_SESSION['username']))
         {
-            return $this->redirectToRoute('ct_homepage');
+            return $this->redirectToRoute('ct_profil');
         } else 
         {
             return $this->redirectToRoute('app_login');
