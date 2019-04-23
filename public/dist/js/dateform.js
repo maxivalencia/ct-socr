90d4ba6c30c3c89a983d65cafb79cde91485a29b
kaira <husaynmozart@gmail.com>
2019-04-23 12:01:42 +0300

    un peu

--- public/dist/js/dateform.js
+++ public/dist/js/dateform.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {    
     //$('.multiselect').select2();   
-    
+    //$(".chosen-select").chosen(); 
     $('.js-datepicker').datepicker({
         format : "yyyy-mm-dd"
     });
