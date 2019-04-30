47d69b4920554fa517293c674c32d15e672744da
kaira <husaynmozart@gmail.com>
2019-04-30 15:48:54 +0300

    historique

--- src/Controller/CtController.php
+++ src/Controller/CtController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Entity\Controles;
 use App\Entity\Roles;
 use App\Entity\Centres;
+use App\Repository\ControlesRepository;
 
 class CtController extends AbstractController
 {
@@ -81,4 +84,56 @@ class CtController extends AbstractController
             'nombrecentre' => $nombre_centre,
         ]);
     }
+
+    /**
+     * @Route("/historique", name="historique_index", methods={"GET"})
+     */
+    public function historique(ControlesRepository $controlesRepository, Request $request): Response
+    {
+        $recherche = $request->query->get('recherche');
+        if($recherche){
+            $controle = $this->getDoctrine()->getRepository(Controles::class)->findBy(['retireur' => null]);
+            $papierrestant = count($controle);
+            $nb_ligne_page = 20;
+            $nombre_page = (int)(ceil($papierrestant / $nb_ligne_page));
+            $numero_page = $request->query->get('page')?(int)$request->query->get('page'):1;
+            return $this->render('ct/historique.html.twig', [
+                'controles' => $controlesRepository->findBy(['Immatriculation' => strtoupper($recherche)], ['id' => 'DESC'], $nb_ligne_page*$numero_page, $numero_page-1),
+                'nombre_page' => $nombre_page,
+                'premiere_page' => 1,
+                'derniere_page' => $nombre_page,
+                'page_precedent' => ($numero_page-1)<1?1:($numero_page-1),
+                'page_suivant' => ($numero_page+1)>=$nombre_page?$nombre_page:($numero_page+1),
+                'numero_page' => $numero_page,
+            ]);
+        }
+        else{
+            $controle = $this->getDoctrine()->getRepository(Controles::class)->findBy(['retireur' => null]);
+            $papierrestant = count($controle);
+            $nb_ligne_page = 20;
+            $nombre_page = (int)(ceil($papierrestant / $nb_ligne_page));
+            $numero_page = $request->query->get('page')?(int)$request->query->get('page'):1;
+            
+            return $this->render('ct/historique.html.twig', [
+                'controles' => $controlesRepository->findAll(['id' => 'DESC'], $nb_ligne_page*$numero_page, $numero_page-1),
+                'nombre_page' => $nombre_page,
+                'premiere_page' => 1,
+                'derniere_page' => $nombre_page,
+                'page_precedent' => ($numero_page-1)<1?1:($numero_page-1),
+                'page_suivant' => ($numero_page+1)>=$nombre_page?$nombre_page:($numero_page+1),
+                'numero_page' => $numero_page,
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/historique/{id}", name="historique_show", methods={"GET"})
+     */
+    public function show(Controles $controle): Response
+    {
+        return $this->render('ct/historique_show.html.twig', [
+            'controle' => $controle,
+        ]);
+    }
+
 }
